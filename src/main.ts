453486c58c0180b41eb67b7dd89787239d0aefa2
Luis Muñoz <lmunozm1702@gmail.com>
2025-06-20 18:28:06 -0400

    Global use for ValidationPipe

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Solo permite propiedades decoradas en el DTO
+      forbidNonWhitelisted: true, // Lanza error si envían datos no permitidos
+      transform: true, // Transforma los payloads al tipo del DTO
+    }),
+  );
+  await app.listen(3000);
 }
 bootstrap();
