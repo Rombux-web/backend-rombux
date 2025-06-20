1c13dec584d0ad442a492841fa80d10dc0bd648d
Luis Muñoz <lmunozm1702@gmail.com>
2025-06-20 18:45:39 -0400

    Import TypeORM module

--- src/raffle/controllers/raffle.controller.ts
+++ src/raffle/controllers/raffle.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+import { RaffleService } from '../services/raffle.service';
+import { CreateRaffleParticipantDto } from '../dto/create-raffle-participant.dto';
+import { RaffleParticipant } from '../entities/raffle-participant.entity';
 
 @Controller('raffle')
-export class RaffleController {}
+export class RaffleController {
+  constructor(private readonly raffleService: RaffleService) {}
+
+  @Post('participate')
+  async participate(
+    @Body() createRaffleParticipantDto: CreateRaffleParticipantDto,
+  ): Promise<RaffleParticipant> {
+    return this.raffleService.create(createRaffleParticipantDto);
+  }
+}
--- src/raffle/raffle.module.ts
+++ src/raffle/raffle.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { RaffleController } from './controllers/raffle.controller';
 import { RaffleService } from './services/raffle.service';
+import { RaffleParticipant } from './entities/raffle-participant.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([RaffleParticipant]), // Import the RaffleParticipant entity for TypeORM
+  ],
   controllers: [RaffleController],
   providers: [RaffleService],
 })
